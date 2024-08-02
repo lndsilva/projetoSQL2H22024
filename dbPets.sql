2bae65bd82e2df13ca187b8a3c0a38059d7b1a28
lndsilva <lndsilva@hotmail.com>
2024-08-02 19:27:53 -0300

    script sql pets

---
+++ dbPets.sql
@@ -0,0 +1,9 @@
+-- apagar o banco de dados
+drop database dbPets;
+-- criar banco de dados
+create database dbPets;
+-- acessar o banco de dados
+use dbPets;
+-- visualizando banco de dados
+show databases;
+
