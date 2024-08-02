9ead6e86fe5402d5080b351f84ba092f46345444
lndsilva <lndsilva@hotmail.com>
2024-08-02 19:49:06 -0300

    script

--- dbPets.sql
+++ dbPets.sql
@@ -6,4 +6,20 @@ create database dbPets;
 use dbPets;
 -- visualizando banco de dados
 show databases;
-
+-- criando tabelas no banco de dados
+create table tbUsuarios(
+codUsu int not null auto_increment,
+nome varchar(50) not null,
+senha varchar(10) not null,
+primary key(codUsu)
+);
+create table tbFuncionarios(
+codFunc int not null auto_increment,
+nome varchar(100) not null,
+email varchar(100),
+telCel char(10),
+);
+-- visualizando tabelas
+show tables;
+-- visualizando a estrutura das tabelas
+desc tbUsuarios;
